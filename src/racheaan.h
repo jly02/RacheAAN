94b2599fa2c0d5bfc9efb41ed335bb75596528ba
Jonathan Ly <lyjonathan02@gmail.com>
2024-02-17 22:22:34 -0800

    Update header file definitions

--- src/racheaan.h
+++ src/racheaan.h
@@ -6,13 +6,30 @@
 #define RACHEAAN_H
 
 using namespace std;
+using namespace seal;
 
+/**
+ * Rache allows the user to customize the poly_modulus_degree and scale
+ * of the encryption scheme. Note that the poly_modulus_degree that is 
+ * chosen has a great effect on the performance of the scheme. 
+ */
 class RacheAAN
 {
-private:
-    /* data */
 public:
-    RacheAAN(size_t poly_modulus_degree = 8192, double scale = pow(2.0, 40));
+    /**
+     * @brief Constructs a new RacheAAN encryption scheme object.
+     * 
+     * @param poly_modulus_degree the degree of the polynomial modulus
+     * @param init_cache_size the initial number of ciphertexts to be cached
+     */
+    RacheAAN(size_t poly_modulus_degree = 8192, int init_cache_size);
+private:
+    vector<Ciphertext> ctxt;
+    Encryptor* enc;
+    Evaluator* eval;
+    Decryptor* dec;
+    CKKSEncoder* encoder;
+    double scale;
 };
 
 #endif
