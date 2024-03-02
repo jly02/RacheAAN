b96f9733d558a3477f84ecabeb611507a008208a
Jonathan Ly <lyjonathan02@gmail.com>
2024-03-01 17:09:45 -0800

    Add simple test

--- src/test/rache_test.cpp
+++ src/test/rache_test.cpp
@@ -1,2 +1,16 @@
 #include "gtest/gtest.h"
 #include "racheaan.h"
+
+using namespace racheaan;
+
+namespace rachetest {
+    TEST(RacheEncryptionTest, HandlesSmallValues) 
+    {
+        Rache rache;
+        seal::Ciphertext destination;
+        EXPECT_NO_THROW(rache.encrypt(   1, destination));
+        EXPECT_NO_THROW(rache.encrypt(  10, destination));
+        EXPECT_NO_THROW(rache.encrypt( 500, destination));
+        EXPECT_NO_THROW(rache.encrypt(1024, destination));
+    }
+} // namespace rachetest
