66260c1994a36ea717c2576093674b90f03794fb
Jonathan Ly <lyjonathan02@gmail.com>
2024-03-02 01:09:47 -0800

    Change name in another file

--- src/test/rache_test.cpp
+++ src/test/rache_test.cpp
@@ -1,7 +1,7 @@
 #include "gtest/gtest.h"
-#include "racheaan.h"
+#include "racheal.h"
 
-using namespace racheaan;
+using namespace racheal;
 
 namespace rachetest {
     // tests encryption of small values works without errors
