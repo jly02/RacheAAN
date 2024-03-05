aa4fa6ea7cd405ee21276966a6540cb3fa9a588e
Jonathan Ly <lyjonathan02@gmail.com>
2024-03-04 23:13:27 -0800

    Reformat bench text

--- src/bench.cpp
+++ src/bench.cpp
@@ -4,25 +4,32 @@ using namespace std;
 
 int main() {
     int selection = 0;
-    cout << "Choose a scheme to benchmark." << endl;
-    cout << "[1 - CKKS | 2 - BFV | 3 - Stream]: ";
-    cin >> selection;
-    switch(selection) {
-        case 1:
-            ckks_bench();
-            break;
 
-        case 2:
-            bfv_bench();
-            break;
+    do {
+        cout << "Choose a demo." << endl;
+        cout << "| 1 - CKKS Benchmark |" << endl
+             << "| 2 -- BFV Benchmark |" << endl
+             << "| 3 - Noise Gen Test |" << endl
+             << "| 0 ----- Exit Demos |" << endl 
+             << "| Selection: ";
+        cin >> selection;
+        switch(selection) {
+            case 1:
+                ckks_bench();
+                break;
 
-        case 3:
-            cipher_stream();
-            break;
+            case 2:
+                bfv_bench();
+                break;
 
-        default:
-            return 0;
-    }
+            case 3:
+                cipher_stream();
+                break;
+
+            default:
+                return 0;
+        }
+    } while (selection != 0);
 
     return 0;
 }
