a97cba9ee7a02486cb0df674455afa15d78bb71e
Jonathan Ly <jly02@cs.washington.edu>
2024-05-23 12:10:02 -0700

    Add datasets and data set runner

--- src/DataSetRunner.cpp
+++ src/DataSetRunner.cpp
@@ -1,28 +1,52 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include "inche.h"
+
+using namespace inche;
 
 void datasets() {
     // Define the file name
-    std::string filename = "covid19";
-    
-    // Create an ifstream object to read from the file
+    std::string filename;
+    std::cout << "Enter file name: ";
+    std::cin >> filename;
     std::ifstream infile(filename);
     
     // Check if the file was opened successfully
     if (!infile.is_open()) {
         std::cerr << "Failed to open file: " << filename << std::endl;
     }
-    
-    // String to hold each line
-    std::string line;
+
+    std::cout << "Reading file... ";
     
     // Read the file line by line
+    std::string line;
+    std::vector<double> vals;
     while (std::getline(infile, line)) {
-        // Process the line (for demonstration, we'll just print it)
-        std::cout << line << std::endl;
+        vals.push_back(std::stod(line));
     }
     
-    // Close the file
     infile.close();
+
+    std::cout << "Done." << std::endl;
+    auto size = vals.size();
+    std::cout << "Size of dataset: " << size << " objects." << std::endl;
+
+    // Test setup
+    std::cout << "Setting up encryption objects... ";
+    Inche inche(seal::scheme_type::ckks);
+    seal::Ciphertext ctxts[size];
+    std::cout << "Done." << std::endl;
+
+    std::cout << "Running data... ";
+    auto start = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < size; i++) {
+        inche.encrypt(vals[i], ctxts[i]);
+    }
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::seconds>(stop - start);
+
+    std::cout << "Done." << std::endl;
+    std::cout << "Took " << duration.count() << " seconds." << std::endl;
 }
