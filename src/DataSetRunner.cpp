e08cb596e8b4a6699d52e92a904bf5bdd25331b1
Jonathan Ly <lyjonathan02@gmail.com>
2024-06-13 19:40:00 -0700

    Update DataSetRunner for all three schemes

--- src/DataSetRunner.cpp
+++ src/DataSetRunner.cpp
@@ -3,8 +3,10 @@
 #include <string>
 #include <vector>
 #include "inche.h"
+#include "racheal.h"
 
 using namespace inche;
+using namespace racheal;
 
 void datasets() {
     // Define the file name
@@ -33,19 +35,80 @@ void datasets() {
     auto size = vals.size();
     std::cout << "Size of dataset: " << size << " objects." << std::endl;
 
+    std::cout << "Choose scheme: [1] CKKS; [2] RacheCKKS; [3] Zinc: ";
+    int scheme;
+    std::cin >> scheme;
+
     // Test setup
     std::cout << "Setting up encryption objects... ";
-    Inche inche(seal::scheme_type::ckks);
     seal::Ciphertext ctxts[size];
-    std::cout << "Done." << std::endl;
+    std::chrono::seconds duration;
+    switch (scheme)
+    {
+    case 1:
+    {
+        seal::EncryptionParameters params(seal::scheme_type::ckks);
+        params.set_poly_modulus_degree(32768);
+    
+        // choose 60 bit primes for first and last (last should just be at least as large as first)
+        // also choose intermediate primes to be close to each other
+        auto coeffs = seal::CoeffModulus::BFVDefault(32768);
+        params.set_coeff_modulus(coeffs);
+    
+        // scale stabilization close to the intermediate primes
+        double scale = pow(2.0, log2(*(coeffs[2].data())));
+    
+        // context gathers params
+        seal::SEALContext context(params);
+        
+        // generate keys
+        seal::KeyGenerator keygen(context);
+        seal::SecretKey secret_key = keygen.secret_key();
+        seal::PublicKey public_key;
+        keygen.create_public_key(public_key);
 
-    std::cout << "Running data... " << std::endl;
-    auto start = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i < size; i++) {
-        inche.encrypt(vals[i], ctxts[i]);
+        // encryptor
+        seal::Encryptor encryptor(context, public_key);
+        // encoder for ckks scheme
+        seal::CKKSEncoder encoder(context);
+
+        std::cout << "Running data... " << std::endl;
+        seal::Plaintext plain;
+        auto start = std::chrono::high_resolution_clock::now();
+        for (int i = 0; i < size; i++) {
+            encoder.encode(vals[i], plain);
+            encryptor.encrypt(plain, ctxts[i]);
+        }
+        auto stop = std::chrono::high_resolution_clock::now();
+        duration = std::chrono::duration_cast<std::chrono::seconds>(stop - start);
     }
-    auto stop = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::seconds>(stop - start);
+    case 2:
+    {
+        Rache rache(seal::scheme_type::ckks, 32);
+
+        std::cout << "Running data... " << std::endl;
+        auto start = std::chrono::high_resolution_clock::now();
+        for (int i = 0; i < size; i++) {
+            rache.encrypt(vals[i], ctxts[i]);
+        }
+        auto stop = std::chrono::high_resolution_clock::now();
+        duration = std::chrono::duration_cast<std::chrono::seconds>(stop - start);
+    }
+    case 3:
+    {
+        Inche inche(seal::scheme_type::ckks);
+
+        std::cout << "Running data... " << std::endl;
+        auto start = std::chrono::high_resolution_clock::now();
+        for (int i = 0; i < size; i++) {
+            inche.encrypt(vals[i], ctxts[i]);
+        }
+        auto stop = std::chrono::high_resolution_clock::now();
+        duration = std::chrono::duration_cast<std::chrono::seconds>(stop - start);
+    }
+    default:
+        break;
+    }
 
     std::cout << "Done." << std::endl;
     std::cout << "Took " << duration.count() << " seconds." << std::endl;
